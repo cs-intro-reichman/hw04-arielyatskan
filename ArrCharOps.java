6ee4dcd90959fe5adbec11f39ae34c9482d6018d
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-11-22 08:20:47 +0200

    Done

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -178,8 +178,8 @@ public class ArrCharOps {
             if(charInt1 > charInt2)  return 1;
             if(charInt1 < charInt2)  return -1;
         }
-        if(str1.length() > str2.length())  return 1;
-        if(str1.length() < str2.length())  return -1;
+        if(str1.length() > str2.length())  return -1;
+        if(str1.length() < str2.length())  return 1;
         return 0;
     }
 }
